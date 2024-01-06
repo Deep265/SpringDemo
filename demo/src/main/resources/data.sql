518d6b0a9aa6584e4105ffc0bbb1c18c0eef7b4e
Esha <102862403+eshadabhade@users.noreply.github.com>
2024-01-06 23:53:27 +0530

    Add files via upload

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee(id, Name, Gender, PhoneNumber) values (101, "Emp 1", "Male", "9876543213");
+insert into employee(id, Name, Gender, PhoneNumber) values (102, "Emp 2", "Male", "9876543216");
+insert into employee(id, Name, Gender, PhoneNumber) values (103, "Emp 3", "Male", "9876543214");
